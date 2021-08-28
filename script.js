91b215843dc40557ed1dc3cfd6bbb13c59d9cf0c
Qj Stout <qjstoutspykers@gmail.com>
2021-08-28 16:15:58 +1000

    Add some dumby Book data for testing

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ import { Book } from './modules/book.js';
 import { Library } from './modules/library.js';
 
 const iliad = new Book('The Iliad', 'Homer', '722');
-const library = new Library();
+const odyssey = new Book('The Odyssey', 'Homer', '961');
+const nineteenEightyFour = new Book('1984', 'George Orwell', '342', true);
 
+const library = new Library();
 library.addBookToLibrary(iliad);
+library.addBookToLibrary(odyssey);
+library.addBookToLibrary(nineteenEightyFour);
 listCatalogue(library);
 
 function listCatalogue(library) {
